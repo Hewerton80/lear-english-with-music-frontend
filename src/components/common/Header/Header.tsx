fa85ada330829858ebce1d7d78ceb71d8a49b4b0
hewerton <hewerton80@gmail.com>
2023-06-03 15:13:47 -0300

    start project

---
+++ src/components/common/Header/Header.tsx
@@ -0,0 +1,27 @@
+import { AppBar, IconButton, useTheme } from "hikari-ui";
+import classNames from "classnames";
+import { FaRegMoon, FaRegSun } from "react-icons/fa";
+
+export function Header() {
+  const { toogleTheme, isDarkMode } = useTheme();
+  return (
+    <AppBar>
+      <AppBar.Tool className={classNames("justify-between mx-auto max-w-7xl")}>
+        <h2
+          className={classNames(
+            "text-base sm:text-2xl text-slate-200 font-bold"
+          )}
+        >
+          Gerador de dados fakes
+        </h2>
+        <div className="flex items-center">
+          <IconButton
+            onClick={toogleTheme}
+            variantStyle="light-ghost"
+            icon={isDarkMode ? <FaRegSun /> : <FaRegMoon />}
+          />
+        </div>
+      </AppBar.Tool>
+    </AppBar>
+  );
+}
