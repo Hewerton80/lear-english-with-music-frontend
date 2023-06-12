68b065ff066b90ae0d8efdbff79d8c9e46b6c7c1
Hewerton Adão <hewerton80@gmail.com>
2023-06-12 19:58:19 -0300

    add form song intergrtion backend

--- src/components/commomPages/FormSong/FormSong.tsx
+++ src/components/commomPages/FormSong/FormSong.tsx
@@ -1,10 +1,13 @@
 "use client";
 import { Button, Card, Input, Select, Textarea, SelectOption } from "hikari-ui";
-import { useCallback, useMemo, useState } from "react";
+import { FormEvent, useCallback, useMemo, useState } from "react";
 import {
-  useGetAuthorsQuery,
+  useGetAuthorsLazyQuery,
   SortOrder,
   GetAuthorsQueryVariables,
+  CreateSongMutation,
+  useCreateSongMutation,
+  MutationCreateSongArgs,
 } from "@/graphql/generated-types";
 import { useDebouncedCallback } from "use-debounce";
 
@@ -18,17 +21,27 @@ const initialAuthorsQueryVariables: GetAuthorsQueryVariables = {
 };
 
 export function FormSong({ songId }: FormSongProps) {
-  const {
-    data: { authors } = {},
-    loading: loadingAuthors,
-    refetch: refetchAuthors,
-  } = useGetAuthorsQuery({
-    variables: initialAuthorsQueryVariables,
-  });
+  const [fetchAuthors, { data, loading: loadingAuthors }] =
+    useGetAuthorsLazyQuery({
+      variables: initialAuthorsQueryVariables,
+    });
+
+  const [createSong, { loading: isCreateTingSong }] = useCreateSongMutation();
 
   const [selectedAuthors, setSelectedAuthors] = useState<SelectOption[]>([]);
+  const [isWriting, setIsWriting] = useState(false);
 
+  const authors = useMemo(() => data?.authors, [data]);
   const isEditAuthor = useMemo(() => Boolean(songId), [songId]);
+  const isLoadingSelect = useMemo(
+    () => loadingAuthors || isWriting,
+    [loadingAuthors, isWriting]
+  );
+  const isSubmitingSong = useMemo(() => isCreateTingSong, [isCreateTingSong]);
+
+  const [title, setTitle] = useState("");
+  const [url, setUrl] = useState("");
+  const [lyric, setLyric] = useState("");
 
   const autocompliteAuthorsOptions = useMemo<SelectOption[]>(() => {
     return (
@@ -42,28 +55,49 @@ export function FormSong({ songId }: FormSongProps) {
   const handleRefetchAuthors = useDebouncedCallback(
     useCallback(
       (newValue: string) => {
-        refetchAuthors({
-          ...initialAuthorsQueryVariables,
-          where: { name: { contains: newValue.trim() } },
+        fetchAuthors({
+          variables: {
+            ...initialAuthorsQueryVariables,
+            where: { name: { contains: newValue.trim() } },
+          },
         });
+        setIsWriting(false);
       },
-      [refetchAuthors]
+      [fetchAuthors]
     ),
     1000
   );
 
   const handleChangeAuthorsInputText = useCallback(
     (newValue: string) => {
-      // if (!newValue.trim()) return;
+      if (!newValue.trim()) return;
+      setIsWriting(true);
       handleRefetchAuthors(newValue);
     },
     [handleRefetchAuthors]
   );
 
+  const getSongFormParams = useCallback(() => {
+    return {
+      authorIds: { values: selectedAuthors.map((authorId) => authorId.value) },
+      data: { title, url, lyric },
+    } as MutationCreateSongArgs;
+  }, [title, url, lyric, selectedAuthors]);
+
   const handleChangeOptions = useCallback((newOptions: SelectOption[]) => {
     if (!newOptions) return;
     setSelectedAuthors(newOptions);
   }, []);
+
+  const handleSubmitSongForm = useCallback(
+    (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      console.log(isEditAuthor);
+      const songFormParams = getSongFormParams();
+      createSong({ variables: songFormParams });
+    },
+    [isEditAuthor, createSong, getSongFormParams]
+  );
 
   return (
     <div className="grid grid-cols-12 gap-4">
@@ -72,13 +106,19 @@ export function FormSong({ songId }: FormSongProps) {
         className="col-span-12 md:col-span-8"
         css={{ overflow: "visible" }}
       >
-        <form action="">
+        <form onSubmit={handleSubmitSongForm}>
           <Card.Header>
             <Card.Title>Criar música</Card.Title>
           </Card.Header>
           <Card.Body>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-              <Input label="Título" placeholder="Easy On Me" required />
+              <Input
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                label="Título"
+                placeholder="Easy On Me..."
+                required
+              />
               <Select
                 label="Bandas/cantore(a)s"
                 value={selectedAuthors}
@@ -88,13 +128,24 @@ export function FormSong({ songId }: FormSongProps) {
                 onInputChange={handleChangeAuthorsInputText}
                 isMulti
                 isAutocomplite
-                isLoading={loadingAuthors || handleRefetchAuthors.isPending()}
+                isLoading={isLoadingSelect}
               />
-              <Textarea label="Letra" />
+              <Input
+                value={url}
+                onChange={(e) => setUrl(e.target.value)}
+                label="Url"
+                placeholder="https://www.youtube.com..."
+                required
+              />
+              <Textarea
+                value={lyric}
+                onChange={(e) => setLyric(e.target.value)}
+                label="Letra"
+              />
             </div>
           </Card.Body>
           <Card.Footer variantStyle="right">
-            <Button>Criar</Button>
+            <Button isLoading={isSubmitingSong}>Criar</Button>
           </Card.Footer>
         </form>
       </Card>
@@ -107,7 +158,9 @@ export function FormSong({ songId }: FormSongProps) {
           <Input label="Nome" placeholder="Adele" required />
         </Card.Body>
         <Card.Footer variantStyle="right">
-          <Button type="submit">Criar</Button>
+          <Button disabled={isSubmitingSong} type="submit">
+            Criar
+          </Button>
         </Card.Footer>
       </Card>
     </div>
