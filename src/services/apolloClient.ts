a6a06b27ffeab6e4e9f5cd72e4d248c4573fddc1
Hewerton Adao da Paz <hewerton.paz@compasso.com.br>
2023-06-21 15:59:30 -0300

    update

--- src/services/apolloClient.ts
+++ src/services/apolloClient.ts
@@ -1,8 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-console.log("import.meta.env.PUBLIC_API", import.meta.env);
 export const apolloClient = new ApolloClient({
   uri: import.meta.env.VITE_PUBLIC_API,
-  // uri: 'http://localhost:3001/',
   cache: new InMemoryCache(),
 });
